830ebf7977218596176a240431ad3a9b9d25615a
gebeen-dev <rrdd1207@gmail.com>
2025-02-13 10:38:57 +0900

    feat : 커스텀 로그인 생성 및 설정

--- src/main/java/com/sparta/test_security/config/SecurityConfig.java
+++ src/main/java/com/sparta/test_security/config/SecurityConfig.java
@@ -11,24 +11,28 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
 		http
-			// HTTP 요청에 대한 접근 권한을 설정
 			.authorizeHttpRequests((auth) -> auth
-				// 루트("/") 및 "/login" 경로는 모든 사용자(비인증 사용자 포함)에게 허용
-				.requestMatchers("/", "/login").permitAll()
-
-				// "/admin" 경로는 "ADMIN" 역할을 가진 사용자만 접근 가능
+				.requestMatchers("/", "/login", "/loginProc").permitAll()
 				.requestMatchers("/admin").hasRole("ADMIN")
-
-				// "/my/**" 경로는 "ADMIN" 또는 "USER" 역할을 가진 사용자만 접근 가능
 				.requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
-
-				// 위에서 설정한 요청 경로 외의 모든 요청은 인증된 사용자만 접근 가능
 				.anyRequest().authenticated()
 			);
 
+
+		http
+			.formLogin((auth) -> auth.loginPage("/login")
+				.loginProcessingUrl("/loginProc")
+				.permitAll()
+			);
+
+		http
+			.csrf((auth) -> auth.disable());
+
+
 		return http.build();
 	}
+
 }
