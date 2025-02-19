a644aed88d7820c336e47eb9f7555f631b673c8b
gebeen-dev <rrdd1207@gmail.com>
2025-02-19 16:04:36 +0900

    feat : UserRepositoty 생성

---
+++ src/main/java/com/sparta/test_security/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.test_security.repository;
+
+import com.sparta.test_security.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
