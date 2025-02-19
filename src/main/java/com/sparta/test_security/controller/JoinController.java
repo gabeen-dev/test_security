4931316e2f2bc42d539e7d3639bf4fa2aef92fd2
gebeen-dev <rrdd1207@gmail.com>
2025-02-19 15:36:20 +0900

    feat : JoinService 생성

--- src/main/java/com/sparta/test_security/controller/JoinController.java
+++ src/main/java/com/sparta/test_security/controller/JoinController.java
@@ -1,12 +1,17 @@
 package com.sparta.test_security.controller;
 
 import com.sparta.test_security.dto.JoinDTO;
+import com.sparta.test_security.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JoinController {
+
+	@Autowired
+	private JoinService joinService;// 추후 생성자 주입방식으로 변경 필요
 
 	@GetMapping("/join")
 	public String joinP() {
@@ -19,6 +24,8 @@ public class JoinController {
 	public String joinProcess(JoinDTO joinDTO) {
 
 		System.out.println(joinDTO.getUsername());
+
+		joinService.joinProcess(joinDTO);
 
 		return "redirect:/login";
 	}
---
+++ src/main/java/com/sparta/test_security/service/JoinService.java
@@ -0,0 +1,12 @@
+package com.sparta.test_security.service;
+
+import com.sparta.test_security.dto.JoinDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+	public void joinProcess(JoinDTO joinDTO) {
+
+	}
+}
