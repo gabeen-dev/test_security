7ccbdfe6b68932b3d91743f7b6edc9547ba5d6fb
gebeen-dev <rrdd1207@gmail.com>
2025-02-19 15:05:18 +0900

    feat : joinDto 생성

--- src/main/java/com/sparta/test_security/controller/JoinController.java
+++ src/main/java/com/sparta/test_security/controller/JoinController.java
@@ -1,5 +1,6 @@
 package com.sparta.test_security.controller;
 
+import com.sparta.test_security.dto.JoinDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,9 @@ public class JoinController {
 
 
 	@PostMapping("/joinProc")
-	public String joinProcess() {
+	public String joinProcess(JoinDTO joinDTO) {
+
+		System.out.println(joinDTO.getUsername());
 
 		return "redirect:/login";
 	}
