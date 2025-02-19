69889ae74ee5d4e8e67d38840ea22e5ea4def73b
gebeen-dev <rrdd1207@gmail.com>
2025-02-19 16:31:49 +0900

    feat : Service: user 정보 저장 메서드 구현

--- src/main/java/com/sparta/test_security/service/JoinService.java
+++ src/main/java/com/sparta/test_security/service/JoinService.java
@@ -1,12 +1,31 @@
 package com.sparta.test_security.service;
 
 import com.sparta.test_security.dto.JoinDTO;
+import com.sparta.test_security.entity.UserEntity;
+import com.sparta.test_security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JoinService {
 
+	@Autowired
+	private UserRepository userRepository;//생성자 주입방식으로 변경
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	public void joinProcess(JoinDTO joinDTO) {
 
+		UserEntity data = new UserEntity();
+
+		//DB에 동일한 Username 있는 지 검증 필요
+		data.setUsername(joinDTO.getUsername());
+		data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+		data.setRole("ROLE_USER");
+
+		userRepository.save(data);
 	}
 }
