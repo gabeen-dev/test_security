4e0fabe633d5feedbf3c2dde2a13822d1b4d7b54
gebeen-dev <rrdd1207@gmail.com>
2025-02-24 11:14:47 +0900

    feat : userRepository username 조회 쿼리 및 service구현

--- src/main/java/com/sparta/test_security/service/CustomUserDetailsService.java
+++ src/main/java/com/sparta/test_security/service/CustomUserDetailsService.java
@@ -1,5 +1,10 @@
 package com.sparta.test_security.service;
 
+import com.sparta.test_security.dto.CustomUserDetails;
+import com.sparta.test_security.entity.UserEntity;
+import com.sparta.test_security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,9 +13,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+	@Autowired
+	private UserRepository userRepository;//생성자 주입 방식으로 변경
+
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		UserEntity userData = userRepository.findByUsername(username);
+
+		if (userData != null) {
+			return new CustomUserDetails(userData);
+		}
 		return null;
 	}
-	
 }
