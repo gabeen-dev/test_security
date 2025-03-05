2a943e28df7e1f0997ba352ae142744503fdf029
gebeen-dev <rrdd1207@gmail.com>
2025-03-05 09:12:55 +0900

    feat : 세션 정보 확인 구현
    role admin

--- src/main/java/com/sparta/test_security/service/CustomUserDetailsService.java
+++ src/main/java/com/sparta/test_security/service/CustomUserDetailsService.java
@@ -4,7 +4,6 @@ import com.sparta.test_security.dto.CustomUserDetails;
 import com.sparta.test_security.entity.UserEntity;
 import com.sparta.test_security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
