30b0d5fc98fbc631267a8a95dce26cf2f9da3715
gebeen-dev <rrdd1207@gmail.com>
2025-03-05 09:11:28 +0900

    fix : role admin 테스트용 수정

--- src/main/java/com/sparta/test_security/service/JoinService.java
+++ src/main/java/com/sparta/test_security/service/JoinService.java
@@ -30,7 +30,7 @@ public class JoinService {
 		//DB에 동일한 Username 있는 지 검증 필요
 		data.setUsername(joinDTO.getUsername());
 		data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
-		data.setRole("ROLE_USER");
+		data.setRole("ROLE_ADMIN");
 
 		userRepository.save(data);
 	}
